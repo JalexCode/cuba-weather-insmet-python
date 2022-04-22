aaca805cd61b5f95d2144da1b211f0ec8ed3fe0e
JalexCode <javierglez9910@gmail.com>
2022-04-22 12:50:53 -0400

    Refractoring, new methods was added

--- cuba_weather_insmet/repositories/weather_repository.py
+++ cuba_weather_insmet/repositories/weather_repository.py
@@ -1,18 +1,33 @@
-from ..insmet_webparser import get_forecast
+from cuba_weather_insmet.insmet_webparser import get_weather_values_map, get_temperatures_map
+
+from ..insmet_webparser import get_forecast, get_weather_values_map, get_weather_state_map
 
 class WeatherRepository:
   '''
     Class to provide the functionality of obtaining weather data
   '''
   def __init__(self):
-    self.weatherdata = get_forecast()
+    pass
 
-  def getWeather(self, source):
+  def get_extended_weather(self, source):
+    weatherdata = get_forecast()
     return WeatherModel(
-      date = self.weatherdata.date, 
+      date = weatherdata.date,
       cityName=source.name,
-      data=self.weatherdata.data[source.name]
+      data= weatherdata.data[source.name]
     )
+
+  # added by @JalexCode
+  def get_weather_values_map(self):
+    return get_weather_values_map()
+
+  # added by @JalexCode
+  def get_temperatures_map(self):
+    return get_temperatures_map()
+
+  # added by @JalexCode
+  def get_weather_state_map(self):
+    return get_weather_state_map()
 
 class WeatherModel():
   def __init__(self, date, cityName, data):
