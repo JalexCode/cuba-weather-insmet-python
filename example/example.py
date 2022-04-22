9f4750a9096a829b90592d1d66e1ed637511195d
JalexCode <javierglez9910@gmail.com>
2022-04-22 13:09:28 -0400

    All new methods was tested

--- example/example.py
+++ example/example.py
@@ -1,7 +1,33 @@
 from cuba_weather_insmet import CubaWeatherInsMet
 
+
 api = CubaWeatherInsMet()
 
-weather = api.get('Santiago')
-
+# extended weather
+print("Pronóstico extendido")
+weather = api.get_extended_weather('Santiago')
 print(weather)
+print("==================================================")
+
+# all temperature values
+print("Mapa de temperaturas")
+tmp_general = api.get_temperatures_map()
+print(tmp_general)
+print("==================================================")
+
+# get national weather
+print("Clima en todo el país")
+national_weather = api.get_weather_values_map()
+for i in national_weather.keys():
+    print(i)
+    print(national_weather[i])
+print("==================================================")
+
+# get weather state
+print("Estado del clima en el territorio nacional")
+state = api.get_weather_state_map()
+for i in state.keys():
+    print(i)
+    for j in state[i]:
+        print(j)
+        print(state[i][j])
